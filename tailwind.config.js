053139147218c3e735889226b7c93dc7e709d65e
swapdi <j.gramm95@googlemail.com>
2025-07-16 01:01:10 +0200

    remove daisy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,5 @@
 export default {
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
-  daisyui: {
-    styled: true,
-    themes: ['acid', 'night'],
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-    prefix: '',
-    darkTheme: 'night'
-  },
+  plugins: [require('@tailwindcss/typography')],
   theme: {
     extend: {
       animation: {
